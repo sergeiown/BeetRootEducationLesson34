30e93b6db296b665e216210f9f489c04112c6921
Serhii Myshko <sergeiown@gmail.com>
2022-12-21 14:59:10 +0200

    add random list generator

--- js/container.js
+++ js/container.js
@@ -1,4 +1,34 @@
-import * as minimum from "./minimum.js";
-import * as normal from "./normal.js";
-import * as maximum from "./maximum.js";
+"use strict";
 
+import * as normal from "./normal.js";
+
+const shoppingList = [];
+
+const preProductList = [
+  "analgi",
+  "aspi",
+  "mega",
+  "supe",
+  "giga",
+  "nitro",
+  "dihydro",
+];
+const postProductList = ["zin", "rin", "ron", "mol", "ran", "ter", "ton"];
+const randomProduct =
+  preProductList[Math.floor(Math.random() * preProductList.length)] +
+  postProductList[Math.floor(Math.random() * postProductList.length)];
+const randomIsBought = Math.random() >= 0.5;
+const randomQuantity = (Math.random() * 9 + 1).toFixed(0);
+const randomPrice = Math.random() * 100;
+
+function addNewItem(product, isBought, quantity, price) {
+  let listItem = new normal.Item(product, isBought, quantity, price);
+  shoppingList.push(listItem);
+}
+
+for (let index = 0; index < (Math.random() * 5 + 5).toFixed(0); index++) {
+  addNewItem(randomProduct, randomIsBought, randomQuantity, randomPrice);
+}
+
+console.log(shoppingList);
+console.log(shoppingList.length);
--- js/normal.js
+++ js/normal.js
@@ -1,1 +1,11 @@
-export {};
+"use strict";
+
+function Item(product, isBought, quantity, price) {
+  this.product = String(product);
+  this.isBought = Boolean(isBought);
+  this.quantity = Number(Math.round(quantity));
+  this.price = Number(price.toFixed(2));
+  this.sum = Number((price * quantity).toFixed(2));
+}
+
+export { Item };
