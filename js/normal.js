8287bfab3f64b334866443648265037618e94c22
Serhii Myshko <sergeiown@gmail.com>
2022-12-21 15:53:29 +0200

    finish with the random shopping list

--- js/normal.js
+++ js/normal.js
@@ -1,5 +1,18 @@
 "use strict";
 
+const shoppingList = [];
+
+const preProductList = [
+  "analgi",
+  "aspi",
+  "mega",
+  "supe",
+  "giga",
+  "nitro",
+  "dihydro",
+];
+const postProductList = ["zin", "rin", "ron", "mol", "ran", "ter", "ton"];
+
 function Item(product, isBought, quantity, price) {
   this.product = String(product);
   this.isBought = Boolean(isBought);
@@ -8,4 +21,23 @@ function Item(product, isBought, quantity, price) {
   this.sum = Number((price * quantity).toFixed(2));
 }
 
-export { Item };
+function addNewItem(product, isBought, quantity, price) {
+  let listItem = new Item(product, isBought, quantity, price);
+  return shoppingList.push(listItem);
+}
+
+function createRandomShoppingList() {
+  for (let index = 0; index < (Math.random() * 5 + 5).toFixed(0); index++) {
+    const randomProduct =
+      preProductList[Math.floor(Math.random() * preProductList.length)] +
+      postProductList[Math.floor(Math.random() * postProductList.length)];
+    const randomIsBought = Math.random() >= 0.5;
+    const randomQuantity = (Math.random() * 9 + 1).toFixed(0);
+    const randomPrice = Math.random() * 100;
+
+    addNewItem(randomProduct, randomIsBought, randomQuantity, randomPrice);
+  }
+  return shoppingList;
+}
+
+export { Item, addNewItem, createRandomShoppingList };
