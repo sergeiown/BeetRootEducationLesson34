49fb3c9d17cfea7159ad5f296aa88dbd935581bf
Serhii Myshko <sergeiown@gmail.com>
2022-12-25 23:54:55 +0200

    add tooltips for buttons etc.

--- js/container.js
+++ js/container.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const STORAGE = {};
+STORAGE.list = [];
+
 import * as creation from "./random_list_creation.js";
 import * as sort from "./list_sorting.js";
 import * as change from "./list_changing.js";
@@ -9,9 +12,25 @@ const buttonNewList = document.querySelector(".new-list");
 
 function createNewList() {
   let shoppingList = creation.returnCompleteShoppingList();
-  const list = document.querySelector(".list");
+  let container = document.querySelector(".list");
+  let htmlData = output.createTableWithListItems(shoppingList);
 
-  list.innerHTML = output.createTableWithListItems(shoppingList);
+  setTimeout(
+    () => (container.innerHTML = htmlData),
+    500
+  ); /* Add a new shoppling list to the page */
+
+  setTimeout(() => (document.querySelector(".sorting").disabled = false), 2000);
+  setTimeout(
+    () => (document.querySelector(".filtering").disabled = false),
+    2500
+  );
+  setTimeout(
+    () => (document.querySelector(".new-item").disabled = false),
+    3000
+  ); /* Make control-buttons visible at the first callback */
+
+  STORAGE.list = shoppingList;
 }
 
 function buyListItem() {}
@@ -19,4 +38,5 @@ function buyListItem() {}
 function removeListItem() {}
 
 // ivent listeners
+
 buttonNewList.addEventListener("click", createNewList);
