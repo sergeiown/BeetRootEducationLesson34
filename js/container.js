ce56d30bf56a7c9d1217e11bc182dabfbc5c799b
Serhii Myshko <sergeiown@gmail.com>
2022-12-27 22:40:53 +0200

    add maximum list size warning

--- js/container.js
+++ js/container.js
@@ -27,7 +27,7 @@ function createNewList() {
   setTimeout(
     () =>
       (htmlContainer.style.background =
-        "url(../img/drugstore.png) bottom right / 50% no-repeat"),
+        "url(../img/drugstore.png) bottom right / 45% no-repeat"),
     500
   ); /* Change background with the delay */
 
@@ -64,13 +64,13 @@ function showActionFormBuyItem(sourceArrayIndex) {
       currency: "USD",
     }
   )} ?</h1>
-        <button class="action-form-yes"></button
-        ><button class="action-form-no"></button>
+        <button class="action-form-yes" id="yes"></button
+        ><button class="action-form-no" id="no"></button>
       </div>
     </div>
   `;
 
-  htmlContainer.addEventListener("mousedown", function (event) {
+  htmlContainer.addEventListener("click", function (event) {
     if (event.target.classList.contains("action-form-yes")) {
       htmlContainer.style.display = "none";
       htmlContainer.innerHTML = "";
@@ -95,7 +95,6 @@ function buyItem(sourceArray, sourceArrayIndex) {
   htmlContainer.innerHTML =
     htmlData; /* Add updated shoppling list to the page */
 }
-//-----Buy item section
 
 //-----Remove item section
 function showActionFormRemoveItem(sourceArrayIndex) {
@@ -104,15 +103,15 @@ function showActionFormRemoveItem(sourceArrayIndex) {
 
   htmlContainer.style.display = "flex";
   htmlContainer.innerHTML = `
-<div class="action-form">
-  <div class="remove-item">
-    <h1>Do you really want to remove ${sourceArray[sourceArrayIndex].quantity} unit(s) of ${sourceArray[sourceArrayIndex].product} from list?</h1>
-    <button class="action-form-yes"></button
-    ><button class="action-form-no"></button>
-  </div>
-</div>`;
+    <div class="action-form">
+      <div class="remove-item">
+        <h1>Do you really want to remove ${sourceArray[sourceArrayIndex].quantity} unit(s) of ${sourceArray[sourceArrayIndex].product} from list?</h1>
+        <button class="action-form-yes" id="yes"></button
+        ><button class="action-form-no" id="no"></button>
+      </div>
+    </div>`;
 
-  htmlContainer.addEventListener("mousedown", function (event) {
+  htmlContainer.addEventListener("click", function (event) {
     if (event.target.classList.contains("action-form-yes")) {
       htmlContainer.style.display = "none";
       htmlContainer.innerHTML = "";
@@ -137,64 +136,113 @@ function removeItem(sourceArray, sourceArrayIndex) {
   htmlContainer.innerHTML =
     htmlData; /* Add updated shoppling list to the page */
 }
-//-----Remove item section
 
 //-----Add new item section
 function showActionFormNewItem() {
   const sourceArray = JSON.parse(JSON.stringify(STORAGE.list));
-  const htmlContainer = document.querySelector(".action");
 
+  if (sourceArray.length >= 11) {
+    showAttention();
+  } else {
+    const sourceArray = JSON.parse(JSON.stringify(STORAGE.list));
+    const htmlContainer = document.querySelector(".action");
+
+    htmlContainer.style.display = "flex";
+    htmlContainer.innerHTML = `
+      <div class="action-form">
+        <div class="add-item">
+          <h2>Specify the details of the new product:</h2>
+            <form class="new-product">
+              <label for="name">name:</label>
+              <input type="text" class="name" name="name" maxlength="25" placeholder=" up to 25 symbols" required>
+  
+              <label for="quantity">quantity:</label>
+              <input type="number" class="quantity" name="quantity" min="1" max="100" placeholder=" up to 100 units" required>
+  
+              <label for="price">price:</label>
+              <input type="number" class="price" name="price" step="0.01" min="0.01" max="100.00" placeholder=" up to 100.00 USD" required>
+  
+              <input type="submit" class="action-form-yes" id="yes">
+            </form>
+          <button class="action-form-no" id="no"></button>
+        </div>
+      </div>`;
+
+    htmlContainer.addEventListener(
+      "submit",
+      hideActionFormAfterAdding
+    ); /* wait for form submit */
+
+    function hideActionFormAfterAdding() {
+      const newProduct = String(
+        document.querySelector(".name").value
+      ).toLowerCase(); /* lowercase - to avoid multiple products with the same name */
+      const newQuantity = Number(document.querySelector(".quantity").value);
+      const newPrice = Number(document.querySelector(".price").value);
+
+      addNewItem(newProduct, newQuantity, newPrice);
+
+      htmlContainer.style.display = "none";
+      htmlContainer.innerHTML = "";
+      htmlContainer.removeEventListener(
+        "submit",
+        hideActionFormAfterAdding
+      ); /* had to remove the event listener due to double function call!!! */
+    }
+
+    htmlContainer.addEventListener("click", function (event) {
+      if (event.target.classList.contains("action-form-no")) {
+        htmlContainer.style.display = "none";
+        htmlContainer.innerHTML = "";
+      } /* wait for "no" button to be clicked */
+    });
+  }
+}
+
+function showAttention() {
+  const htmlContainer = document.querySelector(".action");
   htmlContainer.style.display = "flex";
   htmlContainer.innerHTML = `
-<div class="action-form">
-  <div class="add-item">
-    <h1>Do you really want to</h1>
-    <button class="action-form-yes"></button
-    ><button class="action-form-no"></button>
-  </div>
-</div>`;
+        <div class="action-form">
+          <div class="add-item-attention">
+            <h1>Maximum size of the shopping list exceeded.</h1>
+            <button class="action-form-yes" id="yes"></button
+            ><button class="action-form-no" id="no"></button>
+          </div>
+        </div>`;
 
-  htmlContainer.addEventListener("mousedown", function (event) {
+  htmlContainer.addEventListener("click", function (event) {
     if (event.target.classList.contains("action-form-yes")) {
       htmlContainer.style.display = "none";
       htmlContainer.innerHTML = "";
-      addNewItem();
-    } else if (event.target.classList.contains("action-form-no")) {
-      htmlContainer.style.display = "none";
-      htmlContainer.innerHTML = "";
-    } /* wait for the "yes" or "no" button to be clicked */
+    } /* wait for the "yes" button to be clicked */
   });
 }
 
-function addNewItem() {
-  const sourceArray = STORAGE.list;
-  const newProduct = "Amphetamine"; /* for testing purpose */
+function addNewItem(newProduct, newQuantity, newPrice) {
+  const sourceArray = JSON.parse(JSON.stringify(STORAGE.list));
   const newIsBought = false;
-  const newQuantity = 1;
-  const newPrice = 10;
-  const sourceArrayWithNewItem = change.addItemToList(
+
+  STORAGE.list = change.addItemToList(
     sourceArray,
     newProduct,
     newIsBought,
     newQuantity,
     newPrice
-  );
-
-  STORAGE.list = sourceArrayWithNewItem; /* update the global array */
+  ); /* update the global array */
 
   const htmlContainer = document.querySelector(".list");
   const htmlData = output.createTableWithListItems(STORAGE.list);
   htmlContainer.innerHTML =
     htmlData; /* Add updated shoppling list to the page */
 }
-//-----Add new item section
 
 // main ivent listeners-----
 buttonNewList.addEventListener("click", createNewList);
 
-buttonNewItem.addEventListener("click", addNewItem);
+buttonNewItem.addEventListener("click", showActionFormNewItem);
 
-tableShoppingList.addEventListener("mousedown", function (event) {
+tableShoppingList.addEventListener("click", function (event) {
   if (event.target.classList.contains("cart")) {
     showActionFormBuyItem(event.target.innerHTML);
   } else if (event.target.classList.contains("bin")) {
