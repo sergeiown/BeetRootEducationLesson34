748c436769a4602c240e8a95364eaa1267cafb09
Serhii Myshko <sergeiown@gmail.com>
2022-12-28 01:32:26 +0200

    add sorting functionality

--- js/container.js
+++ js/container.js
@@ -4,17 +4,16 @@ window.STORAGE = {}; /* Yes, I know it's an anti-pattern */
 STORAGE.list = []; /* Create the global array */
 
 import * as creation from "./random_list_creation.js";
-import * as sort from "./list_sorting.js";
-import * as change from "./list_changing.js";
 import * as output from "./list_output.js";
 import * as addnew from "./form_addnew.js";
 import * as buyremove from "./form_buy_remove.js";
+import * as sort from "./form_sort.js";
 
 const tableShoppingList = document.querySelector(".list");
 const buttonNewList = document.querySelector(".new-list");
+const buttonNewItem = document.querySelector(".new-item");
 const buttonSorting = document.querySelector(".sorting");
 const buttonFiltering = document.querySelector(".filtering");
-const buttonNewItem = document.querySelector(".new-item");
 
 function createNewList() {
   const shoppingList = creation.returnCompleteShoppingList();
@@ -50,6 +49,8 @@ function createNewList() {
 buttonNewList.addEventListener("click", createNewList);
 
 buttonNewItem.addEventListener("click", addnew.showActionFormNewItem);
+
+buttonSorting.addEventListener("click", sort.showActionFormSortItems);
 
 tableShoppingList.addEventListener("click", function (event) {
   if (event.target.classList.contains("cart")) {
--- js/list_sorting.js
+++ js/list_sorting.js
@@ -17,22 +17,18 @@ function filterItemsByKeyValue(
   return filteredItemsByKeyValue;
 }
 
-function sortItemsByKey(
-  sourceArray = shoppingList,
-  key = "product",
-  attribute = true
-) {
+function sortItemsByKey(sourceArray, key, attribute) {
   /* attribute can be false for decrease sorting and true for increase sorting
   key can be any key of Item in the sourceArray */
   let source = JSON.parse(JSON.stringify(sourceArray));
 
   if (attribute) {
-    let byKey = (a, b) => (a[key] > b[key] ? 1 : -1);
+    const byKey = (a, b) => (a[key] > b[key] ? -1 : 1);
     const sortedItemsBykey = source.sort(byKey);
 
     return sortedItemsBykey;
   } else {
-    let byKey = (a, b) => (a[key] > b[key] ? -1 : 1);
+    const byKey = (a, b) => (a[key] > b[key] ? 1 : -1);
     const sortedItemsByKey = source.sort(byKey);
 
     return sortedItemsByKey;
